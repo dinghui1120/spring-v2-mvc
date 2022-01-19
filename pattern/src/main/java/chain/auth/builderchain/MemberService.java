4ac090f575d6f632a2b25100f904aab52923291c
dinghui <243542757@qq.com>
2022-01-19 16:01:09 +0800

    责任链模式

---
+++ pattern/src/main/java/chain/auth/builderchain/MemberService.java
@@ -0,0 +1,18 @@
+package chain.auth.builderchain;
+
+import chain.auth.Member;
+
+/**
+ * Created by Tom.
+ */
+public class MemberService {
+    public void login(String loginName,String loginPass){
+        Handler.Builder builder = new Handler.Builder();
+
+        builder.addHandler(new ValidateHandler())
+                .addHandler(new LoginHandler())
+                .addHandler(new AuthHandler());
+
+        builder.build().doHandler(new Member(loginName,loginPass));
+    }
+}
---
+++ pattern/src/main/java/chain/auth/optimiaze/LoginHandler.java
@@ -0,0 +1,15 @@
+package chain.auth.optimiaze;
+
+
+import chain.auth.Member;
+
+/**
+ * Created by Tom.
+ */
+public class LoginHandler extends Handler {
+    public void doHandler(Member member) {
+        System.out.println("登录成功！");
+        member.setRoleName("管理员");
+        next.doHandler(member);
+    }
+}
